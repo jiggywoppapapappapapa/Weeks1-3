768cbee80b6792206932a7603bac9c943f3698f5
Mark Fernandes <ferna100@shernet.sheridancollege.ca>
2026-01-13 10:14:20 -0500

    Added an animation curve

--- Assets/Sctipts/LinearInterpolation.cs
+++ Assets/Sctipts/LinearInterpolation.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+using Unity.Hierarchy;
 using UnityEngine;
 
 public class LinearInterpolation : MonoBehaviour
@@ -6,6 +8,9 @@ public class LinearInterpolation : MonoBehaviour
     public Transform start;
     public Transform end;
     public float t;
+    public AnimationCurve curve;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,6 +26,8 @@ public class LinearInterpolation : MonoBehaviour
             t = 0;
         }
 
-        transform.position=Vector2.Lerp(start.position, end.position, t); 
+        //transform.position=Vector2.Lerp(start.position, end.position, t);
+        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
+
     }
 }
